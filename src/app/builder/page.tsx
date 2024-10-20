1ed3a48f550f3812b06a41b75acd41a9fd2b7f30
Mamun Hoque <mamun3137@gmail.com>
2024-10-21 04:01:40 +0600

    builder updated

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -11,26 +11,3 @@ export default function BuilderPage() {
         </div>
     );
 }
-
-/*
-if i want to use SSR
-"use client";
-
-import React, { useEffect, useState } from 'react';
-import Builder from '../../lib/builder/src/Builder';
-
-export default function BuilderPage() {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true); // Ensures it only runs on the client
-    }, []);
-
-    if (!isClient) return null; // Prevents rendering on the server
-
-    return (
-        <div className="h-screen">
-            <Builder />
-        </div>
-    );
-}*/
